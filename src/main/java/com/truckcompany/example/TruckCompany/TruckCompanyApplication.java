0aa7c3cf8cea839e68a0bb98d161c591879560fe
Tudor-Cofariu <123991646+Tudor181@users.noreply.github.com>
2023-10-03 17:18:08 +0300

    first commit

---
+++ src/main/java/com/truckcompany/example/TruckCompany/TruckCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.truckcompany.example.TruckCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truckcompany/example/TruckCompany/TruckCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truckcompany.example.TruckCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
