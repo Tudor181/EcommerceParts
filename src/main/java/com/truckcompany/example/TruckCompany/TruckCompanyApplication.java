5b6a0f673c36412d9a9eb8a6bf06f5e51e903113
Tudor-Cofariu <123991646+Tudor181@users.noreply.github.com>
2023-10-03 17:41:08 +0300

    first api method test

--- src/main/java/com/truckcompany/example/TruckCompany/TruckCompanyApplication.java
+++ src/main/java/com/truckcompany/example/TruckCompany/TruckCompanyApplication.java
@@ -2,12 +2,20 @@ package com.truckcompany.example.TruckCompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication // annotation -> let the compiler knows what this class is doing
+@RestController
 public class TruckCompanyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TruckCompanyApplication.class, args);
+	}
+
+	@GetMapping("/root")
+	public String apiRoot() {
+		return "Hello World";
 	}
 
 }
