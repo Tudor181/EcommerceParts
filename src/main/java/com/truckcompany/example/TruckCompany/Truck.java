a351fd7e313d3a8a3da800cce4ecc16e2ce73b9f
Tudor-Cofariu <tudor.alin45@gmail.com>
2023-10-23 00:33:22 +0300

    const MAX_SPEED, get set

--- src/main/java/com/truckcompany/example/TruckCompany/Truck.java
+++ src/main/java/com/truckcompany/example/TruckCompany/Truck.java
@@ -1,15 +1,12 @@
 package com.truckcompany.example.TruckCompany;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
-
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,11 +18,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Truck {
 
+    public static final int MAX_SPEED = 90;
+
     @Id
     private String id;
     @NotBlank
     private String imdbId;
     private String imageId;
+    @NotBlank
     private String manufacturer; // mine
     private String nrOfRegistration;
     private int manufactureYear;
@@ -47,5 +47,17 @@ public class Truck {
         }
         this.driverIds = Collections.emptyList();
     }
+
+    // daca am sa maresc proiectul am sa fac settere manuale care sa verifice dar
+    // cred ca asta voi face in service inainte sa apelez settere..
+    // am setter automat asta l am facut pt exemplu
+
+    // public void setManufacturer(String manufacturer){
+    // this.manufacturer=manufacturer;
+    // }
+
+    // public String getManufacturer() {
+    // return manufacturer;
+    // }
 
 }
