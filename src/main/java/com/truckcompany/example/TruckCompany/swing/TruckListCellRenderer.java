6ff507066ca69bbe7886e2c34e61f0a8cde9dc81
Tudor-Cofariu <tudor.alin45@gmail.com>
2023-10-25 23:38:01 +0300

    new class for render the truck in the list

---
+++ src/main/java/com/truckcompany/example/TruckCompany/swing/TruckListCellRenderer.java
@@ -0,0 +1,23 @@
+package com.truckcompany.example.TruckCompany.swing;
+
+import java.awt.Component;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JList;
+
+import com.truckcompany.example.TruckCompany.Truck;
+
+public class TruckListCellRenderer extends DefaultListCellRenderer {
+    @Override
+    public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected,
+            boolean cellHasFocus) {
+        if (value instanceof Truck) {
+            Truck truck = (Truck) value;
+            String textToDisplay = "Manufacturer: " + truck.getManufacturer() + " | Registration: "
+                    + truck.getNrOfRegistration();
+            return super.getListCellRendererComponent(list, textToDisplay, index, isSelected, cellHasFocus);
+        }
+        return super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+    }
+
+}
